f137b4817a7559c78cedc9db9fa84ff601348f45
Robert Hatcher <Robhatcher14@gmail.com>
2024-11-26 10:43:24 -0600

    Create Routes for P12

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 
 const app = express();
 const { PORT = 3001 } = process.env;
+const mainRouter = require("./routes/index");
 
 mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
 .then(() => {
   console.log("Connected to DB");
 })
 .catch(console.error);
+
+app.use("/", mainRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
---
+++ models/clothingItem.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "The name field is required"],
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    required: true,
+    type: String,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageURL: {
+    required: true,
+    type: String,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  likes: [
+    {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "user",
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.export = mongoose.model("item", clothingItemSchema);
